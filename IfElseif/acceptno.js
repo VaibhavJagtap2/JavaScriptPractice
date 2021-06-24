b703db9b94fac92e1b9b2bcb64864d85d5237909
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-06-24 16:44:31 +0530

    Add:IfElse Probems

---
+++ IfElseif/acceptno.js
@@ -0,0 +1,3 @@
+const prompts = require("prompt-sync")();
+const num1 = parseInt(prompts('Enter the first number '));
+console.log(num1);
---
+++ Min_Max.js
@@ -0,0 +1,13 @@
+var max = 0, min=999;
+for (count = 1; count <= 5; count++) {
+    num = Math.floor(Math.random()*(999-100+1)+100);
+    console.log("Number " + count + " : " + num);
+    if(num > max) {
+        max = num;
+    }
+    if (num < min) {
+        min = num;
+    }
+}
+console.log("Maximum number = "+max);
+console.log("Minimum number = "+min);
